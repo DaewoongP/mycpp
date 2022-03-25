ce9370ec9a7fbc3d5816a02683ed1b3e2630f351
DaewoongP <qsc1432@gmail.com>
2022-03-25 15:03:10 +0900

    visual studio c++ practice

---
+++ 220325/220325/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	std::cout << "Test Output" << std::endl;
+	std::cout << "문자열 출력 가능" << std::endl;
+
+	cout << "std namespace 사용하여 출력" << endl;
+
+	return 0;
+}
